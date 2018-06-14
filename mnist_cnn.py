54ebea40b013707e1a4ce8b12fafb5388458555e
mtgorori <33616505+mtgorori@users.noreply.github.com>
2018-06-14 10:42:24 +0900

    乳がんscikitlearn

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -7,7 +7,7 @@ Gets to 99.25% test accuracy after 12 epochs
 
 from __future__ import print_function
 import keras
-from keras.datasets import mnists
+from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
